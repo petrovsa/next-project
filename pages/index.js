02aa71b24530e268238060e8ee4cabde65537cc8
petrovsa <petrovsa@ukr.net>
2022-04-14 21:30:08 +0300

    second

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -13,7 +14,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Lern 
+          Read{' '}
+          <Link href={"first-post"}>
+            <a>My First Page</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
